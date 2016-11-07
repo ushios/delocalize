c9631fbef9abc6df9dfe9bdb80776ea97ac7a766
UshioShugo <ushio.s@gmail.com>
2016-11-08 00:24:22 +0900

    - add localized file check function

--- lib/delocalize/delocalize.go
+++ lib/delocalize/delocalize.go
@@ -2,6 +2,7 @@ package delocalize
 
 import (
 	"errors"
+	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -30,6 +31,32 @@ func Delocalize(path string) error {
 	return nil
 }
 
+// IsLocalizedFile check file name
+func IsLocalizedFile(path string) bool {
+	if strings.HasSuffix(path, LocalizedFilename) {
+		return true
+	}
+
+	return false
+}
+
+// directories from path
+func directories(path string) ([]os.FileInfo, error) {
+	list, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	dl := []os.FileInfo{}
+	for _, fi := range list {
+		if fi.IsDir() {
+			dl = append(dl, fi)
+		}
+	}
+
+	return dl, nil
+}
+
 // delete path name file
 func delete(path string) error {
 	if !IsLocalizedFile(path) {
@@ -41,12 +68,4 @@ func delete(path string) error {
 	}
 
 	return nil
-}
-
-func IsLocalizedFile(path string) bool {
-	if strings.HasSuffix(path, LocalizedFilename) {
-		return true
-	}
-
-	return false
 }
--- lib/delocalize/delocalize_test.go
+++ lib/delocalize/delocalize_test.go
@@ -1,1 +1,22 @@
 package delocalize
+
+import "testing"
+
+func TestIsLocalizedFile(t *testing.T) {
+	table := []struct {
+		path            string
+		isLocalizedFile bool
+	}{
+		{"~/user/Documents/.localized", true},
+		{"/root/.bash_profile", false},
+	}
+
+	for _, e := range table {
+		result := IsLocalizedFile(e.path)
+		if result != e.isLocalizedFile {
+			t.Errorf("path (%s) expected localized file (%t) but (%t)",
+				e.path, e.isLocalizedFile, result,
+			)
+		}
+	}
+}
