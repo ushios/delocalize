e5b96b8899788fe28f5c7e3b5cae1322f57a1a77
Ushio Shugo <ushio.s@gmail.com>
2016-11-08 09:42:05 +0900

    Add file or directorie functions (#1)
    * add delocalize go files
    
    * add localized file check function
    
    * add settings
    
    * add glide lock.
    
    * add main func

---
+++ lib/delocalize/delocalize.go
@@ -0,0 +1,71 @@
+package delocalize
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+const (
+	// LocalizedFilename has filename
+	LocalizedFilename = ".localized"
+)
+
+var (
+	// ErrThisFileIsNotLocalizedFile is error when delete
+	ErrThisFileIsNotLocalizedFile = errors.New("ErrThisFileIsNotLocalizedFile")
+)
+
+// Delocalize directory with searching
+func Delocalize(path string) error {
+	// list, err := ioutil.ReadDir(path)
+	// if err != nil {
+	// 	return err
+	// }
+	//
+	// for _, fi := range list {
+	//
+	// }
+
+	return nil
+}
+
+// IsLocalizedFile check file name
+func IsLocalizedFile(path string) bool {
+	if strings.HasSuffix(path, LocalizedFilename) {
+		return true
+	}
+
+	return false
+}
+
+// directories from path
+func directories(path string) ([]os.FileInfo, error) {
+	list, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	dl := []os.FileInfo{}
+	for _, fi := range list {
+		if fi.IsDir() {
+			dl = append(dl, fi)
+		}
+	}
+
+	return dl, nil
+}
+
+// delete path name file
+func delete(path string) error {
+	if !IsLocalizedFile(path) {
+		return ErrThisFileIsNotLocalizedFile
+	}
+
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+
+	return nil
+}
