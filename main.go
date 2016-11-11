d0a8e53517a137cebb972f5f11ea1e87d4f7a75c
UshioShugo <ushio.s@gmail.com>
2016-11-11 16:47:48 +0900

    get file path

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"path/filepath"
+)
 
+func main() {
+	wd, err := filepath.Abs(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(wd)
 }
